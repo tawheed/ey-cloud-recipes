85d090860278251dabedf36330f81b55a41c3e78
TK <tk@braintrust.co>
2011-10-06 00:01:37 -0700

    Adding cron for trial reminders

---
+++ cookbooks/tout/recipes/default.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: tout
+# Recipe:: default
+#
+
+if ['solo', 'util'].include?(node[:instance_role])
+  cron "Enqueue Scheduled Tout Pitches" do
+    command "cd /data/Tout/current; RAILS_ENV=production bundle exec rake tout:scheduler"
+    minute "*/5"
+    user "deploy"
+  end
+
+  cron "Enqueue Scheduled Tout Pitches" do
+    command "cd /data/Tout/current; RAILS_ENV=production bundle exec rake trials:remind"
+    hour "12"
+    user "deploy"
+  end
+end
+
+
+if ['solo', 'app', 'app_master', 'util'].include?(node[:instance_role])
+  # We want to make sure that each database.yml file uses the same database in this environment
+  node.engineyard.apps.each do |app|
+    template "/data/#{app.name}/shared/config/database.yml" do
+      # adapter: postgres   => datamapper
+      # adapter: postgresql => active record
+      dbtype = case node.engineyard.environment.db_stack
+               when DNApi::DbStack::Mysql     then node.engineyard.environment.ruby_component.mysql_adapter
+               when DNApi::DbStack::Postgres  then 'postgresql'
+               when DNApi::DbStack::Postgres9 then 'postgresql'
+               end
+
+      owner node.engineyard.environment.ssh_username
+      group node.engineyard.environment.ssh_username
+      mode 0655
+      source "database.yml.erb"
+      variables({
+        :dbuser => node.engineyard.environment.ssh_username,
+        :dbpass => node.engineyard.environment.ssh_password,
+        :dbname => "Tout",
+        :dbhost => node.engineyard.environment.db_host,
+        :dbtype => dbtype,
+        :slaves => node.engineyard.environment.db_slaves_hostnames
+      })
+    end
+  end
+end
