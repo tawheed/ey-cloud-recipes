b5981f565c8ca4a5d33f055c890a2a17c0a9510b
TK <tk@braintrust.co>
2012-09-18 15:12:20 -0700

    Running backups on slave only

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -14,7 +14,7 @@ require_recipe "memcached"
 #require_recipe "authorized_keys"
 
 #uncomment to run the eybackup_slave recipe
-#require_recipe "eybackup_slave"
+require_recipe "eybackup_slave"
 
 #uncomment to run the ssmtp recipe
 #require_recipe "ssmtp"
