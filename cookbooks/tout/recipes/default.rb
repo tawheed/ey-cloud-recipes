322c1bd32b167a8ae88941d063cf2a38ec09a7d4
TK <tk@braintrust.co>
2013-01-01 22:23:11 -0800

    Updating packages for document processing

--- cookbooks/tout/recipes/default.rb
+++ cookbooks/tout/recipes/default.rb
@@ -154,18 +154,18 @@ if['solo', 'util'].include?(node[:instance_role])
       action [:install]
     end
   end  
-end
 
-# Set up GraphicsMagick for DocSplit
-bash "install_graphics_magick" do |variable|
-  user "root"
-  cwd "/tmp"
-  code <<-EOH
-  wget ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/1.3/GraphicsMagick-1.3.15.tar.gz
-  tar -xvf GraphicsMagick-1.3.15.tar.gz
-  cd GraphicsMagick-1.3.15
-  ./configure
-  make
-  sudo make install
-  EOH
+  # Set up GraphicsMagick for DocSplit
+  bash "install_graphics_magick" do |variable|
+    user "root"
+    cwd "/tmp"
+    code <<-EOH
+    wget ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/1.3/GraphicsMagick-1.3.15.tar.gz
+    tar -xvf GraphicsMagick-1.3.15.tar.gz
+    cd GraphicsMagick-1.3.15
+    ./configure
+    make
+    sudo make install
+    EOH
+  end  
 end
