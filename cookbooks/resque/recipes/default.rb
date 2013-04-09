e9c855d8396c9068e7ecfd3717cd75418e3a4f0e
TK <tk@braintrust.co>
2013-04-09 16:27:28 -0700

    Making sure we always redirect to root toutapp domain

--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -9,26 +9,26 @@ if ['solo', 'util'].include?(node[:instance_role])
     not_if { "gem list | grep resque" }
   end
 
-  workers = %w{emailp1 emailp1,emailp3 fileprocessingp2,emailp1,emailp3 fileprocessingp2,emailp1,emailp3 emailp3,emailp1 backgroundp4,backgroundp5 backgroundp5,backgroundp6 longjobsp7}
+  workers = %w{emailp1 emailp1,emailp3 fileprocessingp2,emailp1,emailp3 fileprocessingp2,emailp1,emailp3 emailp3,emailp1 backgroundp4,backgroundp5,backgroundp6 backgroundp5,backgroundp4,backgroundp6 longjobsp7}
   num_workers = workers.length
   
   node[:applications].each do |app, data|
     # Only set it up for the Tout app (not ToutAdmin which occupies the same environment)
     if app == 'Tout' then
-      template "/etc/monit.d/resque_#{app}.monitrc" do 
-      owner 'root' 
-      group 'root' 
-      mode 0644 
-      source "monitrc.conf.erb" 
-      variables({ 
-      :num_workers => num_workers,
-      :app_name => app, 
-      :rails_env => node[:environment][:framework_env] 
-      }) 
-      end
-
       # Don't set up workers in the Redis instance, set it up in the rest of them
-      if(!node[:name].match(/ResqueAndRedis/) or node[:instance_role] == "solo")
+      if(!node[:name].match(/ResqueAndRedis|Cacher/) or node[:instance_role] == "solo")
+        template "/etc/monit.d/resque_#{app}.monitrc" do 
+        owner 'root' 
+        group 'root' 
+        mode 0644 
+        source "monitrc.conf.erb" 
+        variables({ 
+        :num_workers => num_workers,
+        :app_name => app, 
+        :rails_env => node[:environment][:framework_env] 
+        }) 
+        end
+
         count = 0
         workers.each do |worker_queue|
           template "/data/#{app}/shared/config/resque_#{count}.conf" do
@@ -54,6 +54,44 @@ if ['solo', 'util'].include?(node[:instance_role])
             echo "sleep 20 && monit -g #{app}_resque restart all" | at now 
           }
         end
+      elsif(node[:name].match(/Cacher/))
+        template "/etc/monit.d/resque_#{app}.monitrc" do 
+        owner 'root' 
+        group 'root' 
+        mode 0644 
+        source "monitrc.conf.erb" 
+        variables({ 
+        :num_workers => 5,
+        :app_name => app, 
+        :rails_env => node[:environment][:framework_env] 
+        }) 
+        end
+
+        count = 0
+        (1..5).each do
+          template "/data/#{app}/shared/config/resque_#{count}.conf" do
+            owner node[:owner_name]
+            group node[:owner_name]
+            mode 0644
+            source "resque.conf.erb"
+            variables({
+              :queue => 'cache'
+            })
+          end
+          count = count + 1
+        end
+
+        execute "ensure-resque-is-setup-with-monit" do 
+          command %Q{ 
+          monit reload 
+          } 
+        end
+
+        execute "restart-resque" do 
+          command %Q{ 
+            echo "sleep 20 && monit -g #{app}_resque restart all" | at now 
+          }
+        end        
       end
     end
   end
